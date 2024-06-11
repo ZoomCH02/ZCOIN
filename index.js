8507f5f567ee4c28a1da15a2875ca8c67f12bf52
иван чучков <yutub.lol@yandex.ru>
2024-06-11 15:44:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const session = require('express-session');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Используем bodyParser для обработки JSON-запросов
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+// Инициализация сессий
+app.use(session({
+    secret: 'your-secret-key', // Секретный ключ для подписи сессии, используйте свой уникальный ключ
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Инициализация базы данных SQLite
+const db = new sqlite3.Database('./db.db', (err) => { });
+
+app.get('/', (req, res) => {
+    const sessionId = req.sessionID;
+    res.send({ sessionId });
+});
+
+// Получение текущего количества кликов из базы данных для данной сессии
+app.get('/userInfo', (req, res) => {
+    const sessionId = req.sessionID;
+    db.get('SELECT * FROM Sessions s, Levels l WHERE s.session_id = ? AND l.id = s.level', [sessionId], (err, row) => {
+        if (err) {
+            console.error('Ошибка при запросе к базе данных:', err.message);
+            res.status(500).send('Ошибка при запросе к базе данных');
+        } else {
+            const clickCount = row ? row.click_count : 0;
+            res.send({ clickCount, row });
+        }
+    });
+});
+
+//Получение карточек
+app.get('/cards', (req, res) => {
+    db.all('SELECT * FROM Cards', (err, row) => {
+        if (err) {
+            console.error('Ошибка при запросе к базе данных:', err.message);
+            res.status(500).send('Ошибка при запросе к базе данных');
+        } else {
+            res.send({ row });
+        }
+    });
+});
+
+app.post('/updateLevel', (req, res) => {
+    const sessionId = req.sessionID;
+    const level = req.body.lvl;
+
+    db.run('UPDATE Sessions SET level = ? WHERE session_id = ?', [level, sessionId], (err) => {
+        if (err) {
+            console.error('Ошибка при обновлении базы данных:', err.message);
+            res.status(500).send('Ошибка при обновлении базы данных');
+        } else {
+            res.send('Клик успешно обновлен');
+        }
+    });
+
+});
+
+// Увеличение количества кликов для данной сессии и обновление в базе данных
+app.post('/click', (req, res) => {
+    const sessionId = req.sessionID;
+    const clickCount = req.body.clickCount;
+
+    db.get('SELECT * FROM Sessions WHERE session_id = ?', [sessionId], (err, row) => {
+        if (err) {
+            console.error('Ошибка при запросе к базе данных:', err.message);
+            res.status(500).send('Ошибка при запросе к базе данных');
+        } else {
+            if (row) {
+                // Обновляем существующую запись
+                db.run('UPDATE Sessions SET click_count = ? WHERE session_id = ?', [clickCount, sessionId], (err) => {
+                    if (err) {
+                        console.error('Ошибка при обновлении базы данных:', err.message);
+                        res.status(500).send('Ошибка при обновлении базы данных');
+                    } else {
+                        res.send('Клик успешно обновлен');
+                    }
+                });
+            } else {
+                // Создаем новую запись
+                db.run('INSERT INTO Sessions (session_id, click_count, level, clickPrice) VALUES (?, ?, ?, ?)', [sessionId, clickCount, 1, 1], (err) => {
+                    if (err) {
+                        console.error('Ошибка при записи в базу данных:', err.message);
+                        res.status(500).send('Ошибка при записи в базу данных');
+                    } else {
+                        res.send('Клик успешно зарегистрирован');
+                    }
+                });
+            }
+        }
+    });
+});
+
+// Запуск сервера
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
---
+++ public/js.js
@@ -0,0 +1,204 @@
+
+const clickerContainer = document.getElementById('clicker-container');
+const clickCountDisplay = document.getElementById('level-display');
+const nameCity = document.getElementById('nameCity')
+
+var currentLvl = 1;
+var coldun = 0;
+
+// Функция для обновления количества кликов
+function updateClickCount(clickCount) {
+    fetch('/click', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ clickCount })
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+            getClickCount();
+        })
+        .catch(error => console.error('Ошибка при обновлении количества кликов:', error));
+}
+
+function updateLevel(lvl) {
+    fetch('/updateLevel', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ lvl })
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+            getClickCount();
+        })
+        .catch(error => console.error('Ошибка при обновлении количества кликов:', error));
+}
+
+function getClickCount() {
+    fetch('/userInfo')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            clickCountDisplay.textContent = data.clickCount;
+            if (data.row) {
+                nameCity.textContent = data.row.name
+                var backgroundimg = document.getElementById('backgroundimg')
+                backgroundimg.setAttribute('style', `background-image: url("${data.row.img}"); border-radius: 50px;`)
+                currentLvl = data.row.level;
+            }
+            else {
+                nameCity.textContent = 'Белгород';
+                currentLvl = 1;
+            }
+        })
+        .catch(error => console.error('Ошибка при получении количества кликов:', error));
+}
+
+// Отключаем контекстное меню для контейнера
+clickerContainer.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+});
+
+clickerContainer.addEventListener('click', (event) => {
+    coldun++;
+
+    let clickCount = parseInt(clickCountDisplay.textContent) + 1;
+    clickCountDisplay.textContent = clickCount;
+    if (coldun > 20) {
+        coldun = 0;
+        updateClickCount(clickCount);
+    }
+
+    if (clickCount >= 100 && currentLvl == 1) {
+        //Белгород
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+    else if (clickCount >= 1000 && currentLvl == 2) {
+        //Луганск
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+    else if (clickCount >= 5000 && currentLvl == 3) {
+        //Донецк
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+    else if (clickCount >= 50000 && currentLvl == 4) {
+        //Мариупль
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+    else if (clickCount >= 100000 && currentLvl == 5) {
+        //Харьков
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+    else if (clickCount >= 500000 && currentLvl == 6) {
+        //Одесса
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+    else if (clickCount >= 1000000 && currentLvl == 7) {
+        //Киев
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+    else if (clickCount >= 10000000 && currentLvl == 8) {
+        //Польша
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+    else if (clickCount >= 100000000 && currentLvl == 9) {
+        //НАТО
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+    else if (clickCount >= 10000000000 && currentLvl == 10) {
+        //ЗЕМЛЯ
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+    else if (clickCount >= 100000000000 && currentLvl == 11) {
+        //СОЛНЕЧНАЯ СИСТЕМА
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+    else if (clickCount >= 10000000000000 && currentLvl == 12) {
+        //МЛЕЧНЫЙ ПУТЬ
+        currentLvl++;
+        updateLevel(currentLvl);
+    }
+
+    const rect = clickerContainer.getBoundingClientRect();
+    const offsetX = event.clientX - rect.left;
+    const offsetY = event.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const moveX = (offsetX - centerX) / 10;
+    const moveY = (offsetY - centerY) / 10;
+
+    clickerContainer.style.transform = `translate(${moveX}px, ${moveY}px) scale(0.95)`;
+
+    setTimeout(() => {
+        clickerContainer.style.transform = 'translate(0, 0) scale(1)';
+    }, 100);
+
+    // Создаем элемент для отображения значения клика
+    const clickValue = document.createElement('div');
+    clickValue.className = 'click-value';
+    clickValue.textContent = '+1';
+    clickValue.style.left = `${offsetX}px`;
+    clickValue.style.top = `${offsetY}px`;
+
+    clickerContainer.appendChild(clickValue);
+
+    // Удаляем элемент после завершения анимации
+    setTimeout(() => {
+        clickValue.remove();
+    }, 1000);
+});
+
+// Получаем текущее количество кликов при загрузке страницы
+getClickCount();
+getCards();
+
+function getCards() {
+    var cardsPlace = document.getElementById('cards');
+    cardsPlace.innerHTML = `<h2>Загрузка...</h2>`
+
+    fetch('/cards')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const cardElement = document.createElement('div');
+            cardElement.className = 'card mb-4';
+            cardElement.style = "min-width: 18rem;"; // Минимальная ширина карточки
+            cardsPlace.innerHTML = ``
+            data.row.forEach(card => {
+                cardsPlace.innerHTML += `
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="${card.img}" class="card-img-top" alt="Upgrade 2">
+                            <h5 class="card-title">${card.name}</h5>
+                            <p style="color: black" class="card-text">Цена: ${card.price}</p>
+                            <p style="color: black" class="card-text">Плюс: ${card.plus}</p>
+                            <button class="btn btn-primary upgrade-btn">Прокачать</button>
+                        </div>
+                     </div>
+                    `
+            });
+            cardsPlace.appendChild(cardElement);
+
+            console.log(data);
+
+        })
+        .catch(error => console.error('Ошибка при получении карточек:', error));
+}
